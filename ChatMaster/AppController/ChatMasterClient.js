953d528b91e3869edc362c96af8f2d1d486704a1
Abdelrahmanmagdy <abdo_magdy@hotmail.com>
2016-01-23 13:29:25 +0200

    ChatMaster0.1
    The First Commitment

---
+++ ChatMaster/AppController/ChatMasterClient.js
@@ -0,0 +1,26 @@
+﻿var ChatApp = angular.module('ChatApp', []);
+ChatApp.controller('ChatController', function ($scope, ChatService) {
+
+    getChats();
+    function getChats() {
+        ChatService.getChats()
+            .success(function (Chats) {
+                $scope.Chats = Chats;
+                console.log($scope.Chats);
+            })
+            .error(function (error) {
+                $scope.status = 'Unable to load Chat data: ' + error.message;
+                console.log($scope.status);
+            });
+    }
+});
+
+ChatApp.factory('ChatService', ['$http', function ($http) {
+
+    var ChatService = {};
+    ChatService.getChats = function () {
+        return $http.get('/Home/GetChatMasters');
+    };
+    return ChatService;
+
+}]);
---
+++ ChatMaster/Controllers/HomeController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace ChatMaster.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Chat Master";
+
+            return View();
+        }
+
+        public JsonResult GetChatMasters()
+        {
+            return Json("");
+        }
+        public JsonResult GetChatDetails(int ID)
+        {
+            return Json("");
+
+        }
+
+        [HttpPost]
+        public JsonResult InsertChatMessage(string Message, int To)
+        {
+            return Json("");
+        }
+
+    }
+}
